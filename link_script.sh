2ee5b952146447b094c9543bba45b7639dc44b88
Saksham Jain <sakjain92@gmail.com>
2018-03-27 19:53:05 -0400

    Link with local H5Part
    Similar to linking with local HDF5, linking with local H5Part also,
    since the original H5Part uses some of the depreciated functions in HDF5
    which doesn't seem to be supported by VOL infrastructure in HDF5.
    
    Signed-off-by: Saksham Jain <sakjain92@gmail.com>

--- link_script.sh
+++ link_script.sh
@@ -1,6 +1,7 @@
 # Run this script to generate links to already existing hdf5 directory
 # Run this script from top directory
 HDF5_VOL_DIR=/home/saksham/work/cmu/hdf5/deltafs_vol/hdf5
+H5PART_VOL_DIR=/home/saksham/work/cmu/hdf5/deltafs_vol/h5part/
 
 # If false, only removes links
 CREATE_LINKS=1
@@ -34,7 +35,7 @@ do
 
 	if [[ $CREATE_LINKS -ne $zero ]]; then
 		ln -s $file VTK/ThirdParty/hdf5/vtkhdf5/src/$f
-	fi
+    fi
 done
 
 
@@ -47,3 +48,27 @@ do
 		ln -s $file VTK/ThirdParty/hdf5/vtkhdf5/src/$f
 	fi
 done
+
+
+for file in `find $H5PART_VOL_DIR/src/ -maxdepth 1 -name "*.h"`; 
+do 
+	f=$(basename $file)
+	rm  -f ParaViewCore/VTKExtensions/H5PartReader/H5Part/src/$f
+
+	if [[ $CREATE_LINKS -ne $zero ]]; then
+		ln -s $file ParaViewCore/VTKExtensions/H5PartReader/H5Part/src/$f
+	fi
+done
+
+
+for file in `find $H5PART_VOL_DIR/src/ -maxdepth 1 -name "*.c"`; 
+do 
+	f=$(basename $file)
+	rm -f ParaViewCore/VTKExtensions/H5PartReader/H5Part/src/$f
+
+	if [[ $CREATE_LINKS -ne $zero ]]; then
+        ln -s $file ParaViewCore/VTKExtensions/H5PartReader/H5Part/src/$f
+	fi
+done
+
+
